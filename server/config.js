8f1e40fe3d843b234639f7d4ca3b22cba21db576
Zihan Wang <zihan9701@gmail.com>
2022-05-29 16:49:45 +0800

    - [feat] implement github secrets

--- server/config.js
+++ server/config.js
@@ -1,6 +1,12 @@
 const db_config = {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: {
+        host: process.env.DB_HOST,
+        port: 5432,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }
 
 }
 
